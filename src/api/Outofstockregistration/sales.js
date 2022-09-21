5ab08739677b1b55adc9efa17607b37f6472755b
浩源 刘 <15698178812@163.com>
2022-09-21 09:18:31 +0800

    1

--- src/api/Outofstockregistration/sales.js
+++ src/api/Outofstockregistration/sales.js
@@ -17,25 +17,25 @@ export function getSales(id) {
   })
 } */
 
-// 新增售后
+// 新增缺货登记表
 export function addSales(data) {
   return request({
-    url: '/system/Aftersales/aftersalesadd',
+    url: '/system/Outofstockregistrationform/Selloutofwarehouseadd',
     method: 'post',
     data: data
   })
 }
 
-// 修改售后
+// 修改缺货登记表
 export function updateSales(data) {
   return request({
-    url: '/system/Aftersales/aftersalesedit',
+    url: '/system/Outofstockregistrationform/Selloutofwarehouseedit',
     method: 'post',
     data: data
   })
 }
 
-// 删除售后
+// 删除缺货登记表
 export function delSales(data) {
   return request({
     url: '/system/Outofstockregistrationform/SwJsPurchaseinboundremove' ,
@@ -46,7 +46,7 @@ export function delSales(data) {
 //销售订单详情
 export function saleOderDetailss(data) {
   return request({
-    url: '/system/Aftersales/saleOderDetail',
+    url: '/system/Outofstockregistrationform/saleOderDetail',
     method: 'get',
     params: data
   })
