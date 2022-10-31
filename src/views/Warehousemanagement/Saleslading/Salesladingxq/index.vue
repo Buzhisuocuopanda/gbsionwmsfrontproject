17aa70a18fd388ac3d5385abe0aa42d32f5d23de
Tothere <2832586647@qq.com>
2022-10-31 15:56:01 +0800

    提货单质检数量不能修改，去掉订单占用量

--- src/views/Warehousemanagement/Saleslading/Salesladingxq/index.vue
+++ src/views/Warehousemanagement/Saleslading/Salesladingxq/index.vue
@@ -73,8 +73,10 @@
           <!--          </el-table-column>-->
           <el-table-column prop="goodsNum" label="良品数量">
             <template scope="scope">
-              <el-input v-model="scope.row.goodsNum" v-if="checkstatus != 2 && status ==3 ? true : false"
-                oninput="value=value.replace(/[^\d]/g,'')" :readonly="edit == 3 ? true : false"></el-input>
+              <!-- <el-input v-model="scope.row.goodsNum" v-if="checkstatus != 2 && status ==3 ? true : false"
+                oninput="value=value.replace(/[^\d]/g,'')" :readonly="edit == 3 ? true : false"></el-input> -->
+                <el-input v-model="scope.row.goodsNum" v-if="checkstatus != 2 && status ==3 ? true : false"
+                oninput="value=value.replace(/[^\d]/g,'')" readonly></el-input>
               <div v-text="rounding2(scope.row.goodsNum)" v-else></div>
             </template>
 
