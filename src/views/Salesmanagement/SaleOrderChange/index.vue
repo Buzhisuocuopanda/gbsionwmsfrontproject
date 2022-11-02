b0ecce1bbc8bdbf92a4385b4c2d426a5781a4982
Tothere <2832586647@qq.com>
2022-11-02 09:54:35 +0800

    销售订单变更单主页样式

--- src/views/Salesmanagement/SaleOrderChange/index.vue
+++ src/views/Salesmanagement/SaleOrderChange/index.vue
@@ -2,7 +2,7 @@
   <!--销售订单变更单-->
   <div class="app-container">
     <div class="filter-container">
-      <el-form :inline="true" label-width="70px">
+      <el-form :inline="true" label-width="70px" style="flex-grow: 0;">
         <el-form-item label="订单号" class="item-r">
           <el-input v-model="orderNo" class="filter-item" placeholder="订单号" />
         </el-form-item>
@@ -22,7 +22,7 @@
             end-placeholder="结束日期" align="right">
           </el-date-picker>
         </el-form-item>
-        <el-form-item style="margin: -5px -10px 1px 1px">
+        <el-form-item style="margin:0">
           <el-button v-hasPermi="['system:saleChange:list']" class="filter-item" type="primary" icon="el-icon-search"
             style="margin-bottom:0;margin-left: 2em" @click="onSearchs">搜索
           </el-button>
@@ -52,8 +52,8 @@
       </el-form>
       <el-table :data="orderList" :row-style="{ height: '3px' }" :cell-style="{ padding: '2px' }"
         element-loading-text="Loading。。。" width="100%;" height="450" border fit highlight-current-row stripe
-        style="margin-top:1em">
-        <el-table-column fixed label="编号" align="left" prop="orderNo" min-width="136px;" />
+        style="margin-top:1em;flex-grow: 1;">
+        <el-table-column fixed label="编号" align="left" prop="orderNo" min-width="140px;" />
         <el-table-column label="客户订单号" align="left" prop="customerNo" min-width="200px;" />
         <el-table-column label="日期" align="left" prop="orderDate" min-width="100px;" />
         <el-table-column label="客户" align="left" prop="customerName" min-width="260px;" />
@@ -100,7 +100,7 @@
       </el-table>
       <el-pagination :background="true" :page-sizes="[10, 15, 20, 50, 500]" :total="totalItems"
         :current-page.sync="listQuery.pageNum" :page-size.sync="listQuery.pageSize"
-        style="padding-top:25px; padding-left: 20px;float: right" layout="total, sizes, prev, pager, next, jumper"
+        style="padding-top:25px; padding-left: 20px;text-align: right;flex-grow: 0;" layout="total, sizes, prev, pager, next, jumper"
         @size-change="onSearch" @current-change="onSearch" />
 
 
@@ -889,5 +889,11 @@ export default {
 .caozuoxiangqengChange {
   border: 0 !important;
   padding: 0 !important;
+}
+.filter-container{
+  width:100%;
+  display: flex;
+  flex-direction: column;
+  height: calc(93vh - 85px);
 }
 </style>
